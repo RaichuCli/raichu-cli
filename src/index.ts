2f701cec0c56463e2fb3da9685c418020d3f5b58
htoooth <ht.anglenx@gmail.com>
2018-09-04 17:23:39 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import path from 'path'
+
+console.log('huangtao')
+
+console.log('huangt1')
+
+const f = (x: string) => console.log(x)
+
+const z = {a: 'x', y: 'y'}
+
+function a(x: any, y: any, cb) {
+  console.log(x, y)
+}
+
+a('A', 'b', async () => {
+  console.log('huangtao')
+})
+
+const xy = {
+  a: '12',
+  b: 'fda'
+}
+
+a(1, {x: 1, y: 2, z: 1}, function() {
+  console.log(a)
+})
+
+console.log('xf')
